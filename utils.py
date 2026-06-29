92cb41edff8edba4485a1e70a57ff05b453bb612
Econetlogistics <seminorossi23244@gmail.com>
2026-06-29 19:01:04 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
+import random
+from datetime import datetime
 
+def generate_tracking_number():
+    """
+    Example:
+    LGS20260629124587
+    """
+    date = datetime.now().strftime("%Y%m%d")
+    random_number = random.randint(100000, 999999)
+
+    return f"LGS{date}{random_number}"
